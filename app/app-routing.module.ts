1bc6f261820a7246724b16c85ef64969b7994ec7
Vaino Niemi <vaino.niemi@visma.com>
2016-12-07 17:15:57 +0200

    Routing now in own module. Added route for detailed info per hero.

---
+++ app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import {NgModule} from "@angular/core";
+import {Routes, RouterModule} from "@angular/router";
+
+import {HeroesComponent} from "./heroes.component";
+import {DashboardComponent} from "./dashboard.component";
+import {HeroDetailComponent} from "./hero-detail.component";
+
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'detail:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/dashboard'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule {}
