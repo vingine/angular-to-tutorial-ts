1d2c33100ecc96e334277ed8e875d2e94eacf22d
Vaino Niemi <vaino.niemi@visma.com>
2016-12-07 16:24:08 +0200

    Added routing and dashboard and separated code to files.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,55 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule } from "@angular/router";
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from "./app.component";
+import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from "./hero-detail.component";
+import { HeroService } from "./hero.service";
+import { DashboardComponent } from "./dashboard.component";
+
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'detail:id',
+        component: HeroDetailComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: '**',
+        redirectTo: '/dashboard'
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap:    [
     AppComponent
   ]
 })
+
 export class AppModule { }
